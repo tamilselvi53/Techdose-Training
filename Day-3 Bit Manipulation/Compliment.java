80ec5de691e42a22e18852481f975e4130536278
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-08-26 14:33:35 +0530

    Update Compliment.java

--- Day-3 Bit Manipulation/Compliment.java
+++ Day-3 Bit Manipulation/Compliment.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+class Main {
+  public static int _1scompliment(int n) {
+    //negation
+		n = ~n;
+		return n;
+	}
+	public static int _2scompliment(int n) {
+    //negation
+		n = ~n;
+    //Adding 1
+		n &= 1;
+		return n;
+	}
+	public static void main(String[] args) {
+		int n = 13;
+		int ones = _1scompliment(n);
+    int twos = _2scompliment(n);
+		System.out.println(twos);
+	}
+}
