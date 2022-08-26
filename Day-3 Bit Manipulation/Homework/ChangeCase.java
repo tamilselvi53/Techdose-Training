d45b5b841652915cb00fc452b2e8e1ef5ba32578
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-08-26 15:44:11 +0530

    Update and rename changeCases.java to ChangeCase.java

---
+++ Day-3 Bit Manipulation/Homework/ChangeCase.java
@@ -0,0 +1,14 @@
+public static char toUpper(char ch) {
+	    return (char)(ch & (~(1<<5)));
+	}
+	public static char toLower(char ch) {
+	    return (char)(ch | (1<<5));
+	}
+	public static char toToggle(char ch) {
+	    return (char)(ch ^ (1<<5));
+	}
+	public static void main(String[] args) {
+		char ch = 'a';
+		char res = toUpper(ch);
+		System.out.println(res);
+	}
