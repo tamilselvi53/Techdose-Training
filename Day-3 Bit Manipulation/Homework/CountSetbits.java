986f0ee3ee0191920b25d6fe4d28bf63aaaa0ed1
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-08-26 22:01:59 +0530

    Update CountSetbits.java

--- Day-3 Bit Manipulation/Homework/CountSetbits.java
+++ Day-3 Bit Manipulation/Homework/CountSetbits.java
@@ -1,5 +1,7 @@
-//Counting 1s by right shifting n 1 step
-  public static int countSetBits1(int n) {
+import java.util.Scanner;	
+class Main {
+	//Counting 1s by right shifting n 1 step
+	public static int countSetBits1(int n) {
 	    int count = 0;
 	    while (n != 0) {
 	        count += (n & 1);
@@ -7,7 +9,7 @@
 	    }
 	    return count;
 	}
-  //Count by left shiffting 1 
+ 	//Count by left shiffting 1 
 	public static int countSetBits2(int n) {
 	    int count = 0;
 	    for (int i = 1; i <= n; i++) {
@@ -17,8 +19,8 @@
 	        i = i<<1;
 	    }
 	    return count;
-	}
-  //Masking first setbit and counting till n becomes 0
+	}	
+	//Masking first setbit and counting till n becomes 0
 	public static int countSetBits3(int n) {
 	    int count = 0;
 	    while (n != 0) {
@@ -32,3 +34,4 @@
 		int res = countSetBits3(n);
 		System.out.println(res);
 	}
+}
