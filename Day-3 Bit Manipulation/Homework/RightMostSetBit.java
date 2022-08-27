3d66c819502e6283e1d7e1d3c75be1d165b0dce1
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-08-27 02:37:51 -0800

    Update RightMostSetBit.java

--- Day-3 Bit Manipulation/Homework/RightMostSetBit.java
+++ Day-3 Bit Manipulation/Homework/RightMostSetBit.java
@@ -1,6 +1,22 @@
 class Main {
-  public static int main (String[] args) {
+  public static int rightMostSetBit(int n) {
+    return n & (~n + 1):
+  }
+  public static int rightMost1Pos(int n) {
+    int pos = 0;
+    while (n != 0) {
+      if ((n & 1) == 1) {
+        return pos + 1;
+      }
+      pos++;
+    }
+  }
+  public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int res1 = rightMostSetBit(n);
+    int res2 = rightMostSet1Pos(n);
+    System.out.println(res1);
+    System.out.println(res2);
   }
+}
