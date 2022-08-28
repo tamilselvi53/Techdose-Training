671dc72ddffe3ff6e710705360fbc5384e975720
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-08-28 20:58:06 +0530

    Create Swap.java

---
+++ Day-3 Bit Manipulation/Homework/Swap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main
+{
+  public static void swap(int a, int b) {
+    a ^= b;
+    b ^= a;
+    a ^= b;
+    System.out.println (a + " " + b);
+  }
+  
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    swap(a, b);
+    System.out.println (a + " " + b);
+  }
+}
