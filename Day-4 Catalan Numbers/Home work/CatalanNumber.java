5dd873ae88087b5e28eb769802e38788042ffc25
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-09-07 18:37:54 +0530

    Update CatalanNumber.java

--- Day-4 Catalan Numbers/Home work/CatalanNumber.java
+++ Day-4 Catalan Numbers/Home work/CatalanNumber.java
@@ -1,1 +1,28 @@
-
+import java.util.*;
+public class Main
+{
+    //formula for nCr
+    public static int binomialCoeff(int n, int k) {
+        int res = 1;
+        if (k > (n - k)) {
+            k = n - k;
+        }
+        for (int i = 0; i < k; i++) {
+            res *= (n - i);
+            res /= (i + 1);
+        }
+        return res;
+    }
+    //Formula for catalan
+    public static int catalan(int n) {
+        int c = binomialCoeff(2 * n, n);
+        return c / (n + 1);
+    }
+ 
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = catalan(n);
+        System.out.println(ans);
+	}
+}
