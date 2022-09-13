5702197092887a77581eec5ee2b6b5a6a119ea29
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-09-13 20:01:51 +0530

    Create lcm.java

---
+++ BeginnersAssignment/lcm.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+    //T(n): O(log n)
+    public static long gcd(long a, long b) {
+        return (a == 0) ? b : gcd(b % a, a);
+    }
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long g = gcd(a, b);
+        long lcm = (a * b) / g;
+        System.out.println(lcm);
+	}
+}
