a14e7f6b71d53e4da53d1c5c8d29cbe1a6a2a333
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-09-14 13:16:03 +0530

    Create 15.AddIntegerToDecimal.java

---
+++ BeginnersAssignment/15.AddIntegerToDecimal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float d = sc.nextFloat();
+        int n = sc.nextInt();
+        float res = d + n;
+        System.out.println(res);
+	}
+}
