90f39d4d7e477765074769bcc248e9b04d2b4e34
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-10-26 12:20:35 +0530

    Update DesignStackToGetMin.java

--- StackAndQueue/Assignment/DesignStackToGetMin.java
+++ StackAndQueue/Assignment/DesignStackToGetMin.java
@@ -1,30 +1,63 @@
-import java.util.*;
+class Stack {
+    int size;
+    int[] stack;
+    int top;
+    int minElement;
+    Stack(int size) {
+        this.size = size;
+        stack = new int[size];
+        top = -1;
+    }
+    boolean isFull() {
+        return top == size - 1;
+    }
+    boolean isEmpty() {
+        return top == -1;
+    }
+    void push(int val) {
+        if (isFull()) {
+            System.out.println("Stack Overflow");
+        } else if (isEmpty()) {
+            minElement = val;
+            stack[++top] = val;
+        } else if (val < minElement) {
+            stack[++top] = 2 * val - minElement;
+            minElement = val;
+        } else {
+            stack[++top] = val;
+        }
+    }
+    int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return -1;
+        } else if (peek() < minElement) {
+            minElement = 2 * minElement - peek();
+        }
+        return stack[top--];
+    }
+    int peek() {
+        return stack[top];
+    }
+    int getMin() {
+        return minElement;
+    }
+}
 
+//Main function
+import java.util.*;
 public class Main
 {
-    public static int getMin(int[] nums) {
-        Stack<Integer> s1 = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
-        for (int i = 0; i < nums.length; i++) {
-            while (!s1.empty() && s1.peek() < nums[i]) {
-                s2.push(s1.pop());
-            }
-            s1.push(nums[i]);
-            while (!s2.empty()) {
-                s1.push(s2.pop());
-            }
-        }
-        return s1.peek();
-    }
 	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    int n = sc.nextInt();
-	    int[] arr = new int[n];
-	    for (int i = 0; i < n; i++) {
-	        arr[i] = sc.nextInt();
-	    }
-	    int minElement = getMin(arr);
-	    System.out.println(minElement);
-	    
+	    Stack s = new Stack(10);
+	    s.push(6);
+	    s.push(4);
+	    s.push(8);
+	    s.pop();
+	    System.out.println(s.getMin());
+	    s.push(0);
+	    System.out.println(s.getMin());
+	    s.pop();
+	    System.out.println(s.getMin());
 	}
 }
