8dd406ffb8d5f7091453f733ac0b829db71488cb
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-11-01 16:07:31 +0530

    Create FirstUniqueCharacterInString.java

---
+++ Hashing/Assignment/FirstUniqueCharacterInString.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < n; i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
